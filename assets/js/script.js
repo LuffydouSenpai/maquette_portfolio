73cd49c3adb296850f45d68310774be57f4ecac3
LuffydouSenpai <luffydou@outlook.fr>
2022-06-08 10:48:01 +0200

    section2 fait

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,6 +50,21 @@ window.addEventListener(
     }
 )
 
+//correction barre de progesion
+//const bRange = document.getElementsByClassName("bRange");
+//console.dir(bRange[0]);
+//const progress = [90,85,75];
+//window.addEventListener("scroll",()=>{
+//    for (let index = 0; index < bRange.length; index++) {
+//        if(bRange[index].scrollIntoView){
+//            bRange[index].style.width = progress[index]+"%";
+//        }
+//        
+//    }
+//})
+
+//-----------------------------------------------------------------------------------------------
+
 document.body.prepend(frameImg);
 let i = 0;
 while (i < imgElements.length) {
